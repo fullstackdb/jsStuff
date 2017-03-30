a8269d042b915cb1aba482cf3e8fa751c397ac07
fullstackdb <a@web-ui.org>
2017-03-30 15:05:05 +0300

    add methods for string reverse algorithm

--- algorithms/string-reverse/stringReverse.js
+++ algorithms/string-reverse/stringReverse.js
@@ -1,7 +1,16 @@
-function reverseString(str) {
-    var reversedString = '';
-    for(var i = str.length - 1; i >= 0 ; i--) {
-        reversedString += str[i];
+function reverseString(text) {
+    text = text.split('');
+    var len = text.length,
+        halfIndex = Math.floor(len / 2) - 1,
+        revText;
+    for (var i = 0; i <= halfIndex; i++) {
+        revText = text[len - i - 1];
+        text[len - i - 1] = text[i];
+        text[i] = revText;
     }
-    return reversedString;
+    return text.join('');
+}
+
+function reverseStringNatively(text) {
+    return (!text || text.length < 2) ? text : text.split('').reverse().join('');
 }
